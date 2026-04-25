e72c2239b4a3e634242747d74219475a27b6d168
TP.VĨ <vitp3705@ut.edu.vn>
2026-04-25 11:33:16 +0700

    sua config database

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-$host = '127.0.0.1';
+$host = 'localhost';
 $dbname = 'web_ban_hang';
 $username = 'root';
-$password = '';
-$port = 3306;
+$password = '123456';
+$port = 3307;
 
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
@@ -12,4 +12,6 @@ if ($conn->connect_error) {
 }
 
 $conn->set_charset('utf8mb4');
+
+echo "Kết nối thành công!";
 ?>
