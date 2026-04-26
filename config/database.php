c9f4147ad0320178600872b890976831aa86316b
trungdh2951-rgb <trungdh2951@ut.edu.vn>
2026-04-26 23:51:41 +0700

    Revert database port to 3306 and clear password for local XAMPP compatability

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 $host = 'localhost';
 $dbname = 'web_ban_hang';
 $username = 'root';
-$password = '123456';
-$port = 3307;
+$password = '';
+$port = 3306;
 
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
@@ -13,5 +13,5 @@ if ($conn->connect_error) {
 
 $conn->set_charset('utf8mb4');
 
-echo "Kết nối thành công!";
+
 ?>
